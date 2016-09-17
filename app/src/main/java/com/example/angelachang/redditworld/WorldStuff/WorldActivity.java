c76277ee16b49d32c59cb329368e9df98b1dc1e8
Tiger Zhao <tigerzhao.zhao@gmail.com>
2016-09-17 01:59:29 -0400

    changed activity files

--- app/src/main/java/com/example/angelachang/redditworld/WorldStuff/WorldActivity.java
+++ app/src/main/java/com/example/angelachang/redditworld/WorldStuff/WorldActivity.java
@@ -1,16 +1,15 @@
 package com.example.angelachang.redditworld.WorldStuff;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.angelachang.redditworld.R;
+public class WorldActivity extends AppCompatActivity {
 
-/**
- * Created by TigerZhao on 16-09-17.
- */
-public class MainWorld extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_world);
     }
 }
+
+
