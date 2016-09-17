5b87bc3984279c56991d957e88269b21b5378465
changangela <angelachang27@gmail.com>
2016-09-17 19:32:19 -0400

    Set up Post interface.

--- app/src/main/java/com/example/angelachang/redditworld/WorldStuff/Post.java
+++ app/src/main/java/com/example/angelachang/redditworld/WorldStuff/Post.java
@@ -4,13 +4,26 @@ package com.example.angelachang.redditworld.WorldStuff;
  * Created by angelachang on 9/17/16.
  */
 public class Post {
-    private int m_ID;
+    private int m_id;
     private int m_x;
     private int m_y;
+    private String m_title;
+    private String m_url;
+    private int m_score;
+
+    public String getTitle(){
+        return m_title;
+    }
+    public String getUrl(){
+        return m_url;
+    }
+    public int getScore(){
+        return m_score;
+    }
     private String m_image;
 
     public int getID(){
-        return m_ID;
+        return m_id;
     }
 
     public int getX(){
@@ -25,8 +38,8 @@ public class Post {
         return m_image;
     }
 
-    public void setID(int ID){
-        m_ID = ID;
+    public void setID(int id){
+        m_id = id;
     }
 
     public void setX(int x){
@@ -39,5 +52,14 @@ public class Post {
 
     public void setImage(String image){
         m_image = image;
+    }
+
+    public Post(int id, int x, int y, String url, String title, int score){
+        m_id = id;
+        m_x = x;
+        m_y = y;
+        m_url = url;
+        m_title = title;
+        m_score = score;
     }
 }
