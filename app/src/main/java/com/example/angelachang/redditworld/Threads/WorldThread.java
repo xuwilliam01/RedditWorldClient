dbbf9c8a14d3c8e5f964170330b9c386c28504d6
Oscar <ooosssososos@hotmail.com>
2016-09-17 09:33:19 -0400

    Canvas Drawing Worked out

---
+++ app/src/main/java/com/example/angelachang/redditworld/Threads/WorldThread.java
@@ -0,0 +1,64 @@
+package com.example.angelachang.redditworld.Threads;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by oscar on 9/17/16.
+ */
+public class WorldThread extends Thread{
+
+    private boolean running = true;
+
+    private final Object mRunLock = new Object(); // dafuq
+
+    private Paint painter;
+
+    int x = 0;
+
+    private SurfaceHolder mSurfaceHolder;
+
+    public WorldThread(SurfaceHolder surfaceHolder, Context context,
+                       Handler handler) {
+
+        mSurfaceHolder = surfaceHolder;
+        painter = new Paint();
+        painter.setAntiAlias(true);
+        painter.setARGB(255, 0, 255, 0);
+
+    }
+
+    public void run() {
+        while (running) {
+            Canvas c = null;
+            try {
+                c = mSurfaceHolder.lockCanvas(null);
+                synchronized (mSurfaceHolder) {
+                    //game logic
+                    synchronized (mRunLock) { //dafuq does this do? mrunlock is just a generic new obj
+                        if (running) doDraw(c);
+                    }
+                }
+            } finally {
+                //cleaning up so as to not leaving hanging state
+                if (c != null) {
+                    mSurfaceHolder.unlockCanvasAndPost(c);
+                }
+            }
+        }
+    }
+
+    private void doDraw(Canvas canvas) {
+        // Draw the background image. Operations on the Canvas accumulate
+        // so this is like clearing the screen.
+        canvas.drawRect(20,30,x++,50,painter);
+        System.out.println("DEBUG");
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/angelachang/redditworld/Views/WorldView.java
@@ -0,0 +1,46 @@
+package com.example.angelachang.redditworld.Views;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import com.example.angelachang.redditworld.Threads.WorldThread;
+
+/**
+ * Created by oscar on 9/17/16.
+ */
+public class WorldView  extends SurfaceView implements SurfaceHolder.Callback{
+
+    WorldThread thread;
+
+    public WorldView(Context context, AttributeSet attrib) {
+        super(context, attrib);
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+        // create thread only; it's started in surfaceCreated()
+        thread = new WorldThread(holder, context, new Handler() {
+            @Override
+            public void handleMessage(Message m) {
+
+            }
+        });
+        setFocusable(true); // make sure we get key events
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
