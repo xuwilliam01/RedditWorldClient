85006b67c9410818009fa625bcc7044ced65d386
changangela <angelachang27@gmail.com>
2016-09-17 12:16:21 -0400

    Added instance of GameWorld to ServerComThread

--- app/src/main/java/com/example/angelachang/redditworld/Threads/ServerComThread.java
+++ app/src/main/java/com/example/angelachang/redditworld/Threads/ServerComThread.java
@@ -2,6 +2,7 @@ package com.example.angelachang.redditworld.Threads;
 
 import com.example.angelachang.redditworld.R;
 import com.example.angelachang.redditworld.Util.ConfigVals;
+import com.example.angelachang.redditworld.WorldStuff.GameWorld;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,6 +13,7 @@ import java.net.Socket;
  */
 public class ServerComThread extends Thread{
 
+    GameWorld gameWorld = GameWorld.getInstance();
     @Override
     public void run(){
         try{
