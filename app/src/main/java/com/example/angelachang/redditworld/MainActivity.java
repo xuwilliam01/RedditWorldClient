2c97cd82682e00dcfa500e5aff10407e279162da
Tiger Zhao <tigerzhao.zhao@gmail.com>
2016-09-17 02:16:39 -0400

    Added button to main menu that takes the user to the world screen.

--- app/src/main/java/com/example/angelachang/redditworld/MainActivity.java
+++ app/src/main/java/com/example/angelachang/redditworld/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.angelachang.redditworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.angelachang.redditworld.WorldStuff.WorldActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void EnterWorld(View v) {
+        Intent intent = new Intent(this, WorldActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/angelachang/redditworld/WorldStuff/WorldActivity.java
+++ app/src/main/java/com/example/angelachang/redditworld/WorldStuff/WorldActivity.java
@@ -3,6 +3,8 @@ package com.example.angelachang.redditworld.WorldStuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.angelachang.redditworld.R;
+
 public class WorldActivity extends AppCompatActivity {
 
     @Override
