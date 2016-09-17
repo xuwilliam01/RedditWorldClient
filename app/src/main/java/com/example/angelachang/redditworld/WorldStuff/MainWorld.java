56c65f45df83e47baa3c9e48e37f88d94d34b736
Tiger Zhao <tigerzhao.zhao@gmail.com>
2016-09-17 01:23:44 -0400

    initial commit. few files added

---
+++ app/src/main/java/com/example/angelachang/redditworld/WorldStuff/MainWorld.java
@@ -0,0 +1,16 @@
+package com.example.angelachang.redditworld.WorldStuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.angelachang.redditworld.R;
+
+/**
+ * Created by TigerZhao on 16-09-17.
+ */
+public class MainWorld extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world);
+    }
+}
---
+++ app/src/main/java/com/example/angelachang/redditworld/WorldStuff/Player.java
@@ -0,0 +1,11 @@
+package com.example.angelachang.redditworld.WorldStuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by TigerZhao on 16-09-17.
+ */
+public class Player {
+
+}
