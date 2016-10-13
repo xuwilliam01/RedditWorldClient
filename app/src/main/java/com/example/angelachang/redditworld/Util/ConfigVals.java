59f9b2c07ad51a70042f2109519c8056ae56bcc3
changangela <angelachang27@gmail.com>
2016-10-13 14:19:26 -0400

    Uploaded demo of project.

--- app/src/main/java/com/example/angelachang/redditworld/Util/ConfigVals.java
+++ app/src/main/java/com/example/angelachang/redditworld/Util/ConfigVals.java
@@ -5,7 +5,7 @@ package com.example.angelachang.redditworld.Util;
  */
 public class ConfigVals {
 
-    public static final String server_ip = "10.21.100.158";
+    public static final String server_ip = "10.22.166.200";
     public static final int port = 4200;
 
 
